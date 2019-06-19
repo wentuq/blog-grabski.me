b07f7078ffaf17d2ad9618e7dbfe2931d757cbf2
Marcin Grabski <wentuq@gmail.com>
2019-06-19 19:16:55 +0100

    added run script

---
+++ script.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+set -e
+
+bundle exec ./yaml.rb && bundle exec jekyll build
--- yaml.rb
+++ yaml.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # encoding: utf-8
 
 ### FLICKR API environment variables in ~/.profile
